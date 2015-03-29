a3ef80ed93e40d7d1b3baf3ceba50b5042bdd2e1
simoto <simeon.d.cholakov@gmail.com>
2015-03-29 20:07:11 +0300

    added references for all current files and library to the main page

---
+++ app/js/services/notifyService.js
@@ -0,0 +1,35 @@
+'use strict';
+
+app.factory('notifyService',
+    function () {
+        return {
+            showInfo: function(msg) {
+                $.notify( msg, 'info');
+            },
+            showError: function(msg, serverError) {
+                // Collect errors to display from the server response
+                var errors = [];
+                if (serverError && serverError.error_description) {
+                    errors.push(serverError.error_description);
+                }
+                if (serverError && serverError.modelState) {
+                    var modelStateErrors = serverError.modelState;
+                    for (var propertyName in modelStateErrors) {
+                        var errorMessages = modelStateErrors[propertyName];
+                        var trimmedName =
+                            propertyName.substr(propertyName.indexOf('.') + 1);
+                        for (var i = 0; i < errorMessages.length; i++) {
+                            var currentError = errorMessages[i];
+                            errors.push(trimmedName + ' - ' + currentError);
+                        }
+                    }
+                }
+                if (errors.length > 0) {
+                    msg = msg + ":<br>" + errors.join("<br>");
+                }
+
+                $.notify(msg,'error');
+            }
+        }
+    }
+);
