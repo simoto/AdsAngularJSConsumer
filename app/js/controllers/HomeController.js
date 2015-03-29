07f86a474e9dc98b9ea292c1d13c8d3f294cbece
simoto <simeon.d.cholakov@gmail.com>
2015-03-30 01:43:36 +0300

    implement home page to show all ads

--- app/js/controllers/HomeController.js
+++ app/js/controllers/HomeController.js
@@ -3,6 +3,11 @@
 // The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
-        // TODO
+        adsService.getAds(null, function success(data){
+            $scope.ads = data;
+            notifyService.showInfo('Ads added');
+        }, function error(err) {
+            notifyService.showError('Cannot load ads', err);
+        });
     }
 );
--- app/js/services/adsServices.js
+++ app/js/services/adsServices.js
@@ -2,8 +2,14 @@
 
 app.factory('adsService',
     function ($resource, baseServiceUrl) {
+        var adsResource = $resource(baseServiceUrl + '/api/ads', null,
+            {
+                'getAll' : { method: 'GET' }
+            });
         return {
-            // TODO: implement a service to get ads
+            getAds: function(params, success, error){
+                return adsResource.getAll(params, success, error);
+            }
         };
     }
 );
