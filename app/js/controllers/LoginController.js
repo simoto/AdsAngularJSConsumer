b7f8e051be346258aa3c740e5d5ab62158df249c
simoto <simeon.d.cholakov@gmail.com>
2015-03-31 20:48:02 +0300

    added authentication service implementation

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -3,6 +3,21 @@
 // The LoginController is responsible for the "Login" screen
 app.controller('LoginController',
     function ($scope, $location, authService, notifyService) {
-        // TODO
+        $scope.login = function(userData){
+            authService.login(userData,
+                function success(){
+                    notifyService.showInfo("Login successful!");
+                    $location.path('/');
+                },
+                function error(){
+                    notifyService.showError("Login failed!");
+                    $location.path('/login');
+                }
+            );
+        }
+
+        $scope.redirectToRegister = function (){
+            $location.path('/register');
+        }
     }
 );
